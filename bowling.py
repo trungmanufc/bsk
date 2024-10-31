d92c22328bfc1240266960fd475b42cdf784d534
trung <trungmanufc@gmail.com>
2024-10-31 12:08:38 +0200

    story 3

--- bowling.py
+++ bowling.py
@@ -19,12 +19,18 @@ class BowlingGame:
     def calculate_score(self) -> int:
         score = 0
         is_spare = False
-        for frame in self._frames:
+        is_strike = False;
+        for frame in self._frame:
+            if is_strike:
+                score = score + frame.get_first_throw() + frame.get_second_throw()
+                is_strike = False
             if is_spare:
                 score = score + frame.get_first_throw() + frame.get_second_throw();
                 is_spare = False;
             if frame.is_spare():
                 is_spare = True;
+            if frame.is_strike():
+                is_strike = True;
         pass
 
     def set_first_bonus_throw(self, bonus_throw: int) -> None:
--- test/test_bowling.py
+++ test/test_bowling.py
@@ -9,3 +9,17 @@ class TestBowlingGame(unittest.TestCase):
 
     def test_something(self):
         pass
+    def test_calculate_score_strike(self):
+        game = BowlingGame()
+        game.add_frame(Frame(10, 0))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(7, 2))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(4, 4))
+        game.add_frame(Frame(5, 3))
+        game.add_frame(Frame(3, 3))
+        game.add_frame(Frame(4, 5))
+        game.add_frame(Frame(8, 1))
+        f = Frame(2, 6)
+        game.add_frame(f)
+        self.assertEqual(94,game.calculate_score())
